2b86c0ae32f7e58eb88bb9fdb2e3f8daf7e5fa9f
RebecaConte <becaconte@gmail.com>
2021-04-17 22:55:54 +0100

    done

--- routes/drones.js
+++ routes/drones.js
@@ -2,35 +2,80 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+const Drone = require('../models/Drone.model.js')
+
+// *** GETTING/READING A LIST OF DRONES ***
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  Drone.find({})
+  .then((drones) => {
+    console.log(drones);
+    // are we not supposed to provide only the file name, NOT the path? 
+    // lost lot of time trying to figure this out 
+    res.render('drones/list.hbs', {drones})
+  })
+  .catch((err) => {
+    console.log("drones not found")
+  })
 });
+
+// *** CREATE NEW DRONE ***
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/create-form.hbs')
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  const {name, propellers, maxSpeed} = req.body
+  console.log(name, propellers, maxSpeed);
+
+  Drone.create({name, propellers, maxSpeed})
+  .then((data) => {
+    res.redirect('/drones')
+  })
+  .catch((err) => console.log(err))
 });
+
+// *** EDIT A DRONE ***
+
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const {id} = req.params
+
+  Drone.findById(id)
+  .then((data) => {
+    res.render('drones/update-form.hbs', {data})
+  })
+  .catch((err) => console.log(err))
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const {id} = req.params
+  const {name, propellers, maxSpeed} = req.body
+
+  Drone.findByIdAndUpdate(id, {name, propellers, maxSpeed})
+  .then((data) => {
+    res.redirect('/drones')
+  })
+  .catch((err) => console.log(err))
 });
+
+// *** DELETE A DRONE ***
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  const {id} = req.params
+  
+  Drone.findByIdAndDelete(id)
+  .then((data) => {
+    res.redirect('/drones')
+  })
+  .catch((err) => console.log(err))
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,28 @@
 // Iteration #1
+
+// -- connect the DB --
+require('../db')
+
+// -- create Drone items to add to the collection --
+const myDrones = [
+    { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+    { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+    { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+]
+
+// -- insert drone items to the DB --
+
+const mongoose = require('mongoose')
+const Drone = require('../models/Drone.model.js')
+
+Drone.create(myDrones)
+.then(() => {
+    console.log("YAY! Everything working - Super Cool!");
+    //close connection with database
+    mongoose.connection.close()
+})
+.catch((err) => {
+    console.log("There is a problem adding the info: " + err)
+})
+
+
